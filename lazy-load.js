2f855eba79692c3ba8abab0956d2e2952fe39d41
DamirPavlik <damir.pavlik@gmail.com>
2024-12-23 12:04:07 +0100

    feat: add lazy load

---
+++ lazy-load.js
@@ -0,0 +1,19 @@
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return rect.top < window.innerHeight && rect.bottom > 0;
+}
+
+function lazyLoadImages() {
+    const lazyImages = document.querySelectorAll('img[data-src]');
+    lazyImages.forEach(img => {
+        if (isInViewport(img)) {
+            img.src = img.getAttribute('data-src');
+            img.removeAttribute('data-src');
+            img.classList.remove('placeholder');
+        }
+    });
+}
+
+window.addEventListener('load', lazyLoadImages);
+window.addEventListener('scroll', lazyLoadImages);
+window.addEventListener('resize', lazyLoadImages);
